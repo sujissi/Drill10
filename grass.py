d7c6e68e994c4691fb8093561e59fc2645911a8a
sujissi_Laptop <hwangsuji329@gmail.com>
2023-10-19 10:57:10 +0900

    drill #10 제출(2020182045 황수지)

--- grass.py
+++ grass.py
@@ -2,8 +2,8 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self, y_move=0):
-        self.x, self.y = 400,30 + y_move
+    def __init__(self, dir=0):
+        self.x, self.y = 400,30 + dir
         self.image = load_image('grass.png')
 
     def draw(self):
